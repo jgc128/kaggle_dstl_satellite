64c4ae4561af312a5dec0d192e771e3517e7d171
Alexey Romanov <jgc128@outlook.com>
2017-02-26 14:55:38 -0500

    Prediction

--- simple_model_v2.py
+++ simple_model_v2.py
@@ -9,10 +9,10 @@ import tensorflow.contrib.slim as slim
 from tensorflow_helpers.models.base_model import BaseModel
 
 from config import CLASSES_NAMES, IMAGES_NORMALIZED_SHARPENED_FILENAME, IMAGES_MASKS_FILENAME, IMAGES_METADATA_FILENAME, \
-    IMAGES_MEANS_STDS_FILENAME, TENSORBOARD_DIR, MODELS_DIR
-from utils.data import load_pickle, convert_masks_to_softmax, convert_softmax_to_masks
+    IMAGES_MEANS_STDS_FILENAME, TENSORBOARD_DIR, MODELS_DIR, IMAGES_NORMALIZED_DATA_DIR, IMAGES_PREDICTION_MASK_DIR
+from utils.data import load_pickle, convert_masks_to_softmax, convert_softmax_to_masks, get_train_test_images_ids
 from utils.matplotlib import matplotlib_setup
-from utils.polygon import stack_masks, sample_patches, jaccard_coef
+from utils.polygon import stack_masks, sample_patches, jaccard_coef, split_image_to_patches, join_mask_patches
 
 
 class SimpleModel(BaseModel):
@@ -100,7 +100,7 @@ class SimpleModel(BaseModel):
 
         with tf.name_scope('loss'):
             cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=targets_one_hot, logits=logits)
-            loss_ce = tf.reduce_mean(tf.reduce_mean(cross_entropy, axis=[1, 2]))
+            loss_ce = tf.reduce_mean(tf.reduce_sum(cross_entropy, axis=[1, 2]))
 
             tf.losses.add_loss(loss_ce)
             self.op_loss = tf.losses.get_total_loss(add_regularization_losses=True)
@@ -115,7 +115,7 @@ def config():
     nb_epoch = 20
     batch_size = 64
 
-    model_name = 'jaccard_softmax_without_small_pansharpen'
+    model_name = 'softmax_without_small_pansharpen'
 
     classes_to_skip = [9, 10]
     patch_size = [224, 224]
@@ -131,16 +131,17 @@ def config():
     model_load_step = -1
     debug = False
 
+
 @ex.named_config
 def big_objects():
-    model_name = 'jaccard_softmax_without_small_pansharpen_big_objects'
+    model_name = 'softmax_without_small_pansharpen_big_objects'
     classes_to_skip = [2, 5, 9, 10]
     patch_size = [224, 224]
 
 
 @ex.named_config
 def small_objects():
-    model_name = 'jaccard_softmax_without_small_pansharpen_small_objects'
+    model_name = 'softmax_without_small_pansharpen_small_objects'
     classes_to_skip = [1, 3, 4, 6, 7, 8]
     patch_size = [64, 64]
 
@@ -154,9 +155,17 @@ def debug_run():
     nb_samples_val = 5
     batch_size = 5
 
+
+@ex.named_config
+def prediction():
+    model_load_step = 20700
+    model_name = 'softmax_without_small_pansharpen_big_objects_small_patch'
+    patch_size = [64, 64]
+    batch_size = 350
+
+
 @ex.capture
 def evaluate_model_jaccard(model, images, images_masks_stacked, images_data, needed_classes, kind, batch_size):
-
     data_dict = sample_data_dict(images, images_masks_stacked, images_data, kind)
     nb_samples = len(data_dict['X'])
 
@@ -173,7 +182,6 @@ def evaluate_model_jaccard(model, images, images_masks_stacked, images_data, nee
 @ex.capture
 def sample_data_dict(images, images_masks_stacked, images_data, kind,
                      patch_size, nb_samples_train, nb_samples_val, val_size, classes_to_skip):
-
     if kind == 'val':
         nb_samples = nb_samples_val
     elif kind == 'train':
@@ -191,8 +199,9 @@ def sample_data_dict(images, images_masks_stacked, images_data, kind,
 
     return data_dict
 
+
 @ex.main
-def main(model_name, classes_to_skip, patch_size, nb_iterations, batch_size, debug, regularization):
+def main(model_name, classes_to_skip, patch_size, nb_iterations, batch_size, debug, regularization, model_load_step):
     # set-up matplotlib
     matplotlib_setup()
 
@@ -200,6 +209,9 @@ def main(model_name, classes_to_skip, patch_size, nb_iterations, batch_size, deb
     nb_classes = len(classes_names)
     classes = np.arange(1, nb_classes + 1)
     logging.info('Classes: %s', nb_classes)
+
+    images_all, images_train, images_test = get_train_test_images_ids()
+    logging.info('Train: %s, test: %s, all: %s', len(images_train), len(images_test), len(images_all))
 
     # load images data
     images_data = load_pickle(IMAGES_NORMALIZED_SHARPENED_FILENAME)
@@ -254,49 +266,100 @@ def main(model_name, classes_to_skip, patch_size, nb_iterations, batch_size, deb
     model.build_model()
 
     # train model
+    if model_load_step == -1:
 
-    iteration_number = 0
-    jaccard_val_mean = 0
-    jaccard_train_mean = 0
-    for iteration_number in range(1, nb_iterations + 1):
-        try:
-            data_dict_train = sample_data_dict(images, images_masks_stacked, images_data, 'train')
-            model.train_model(data_dict_train, nb_epoch=1, batch_size=batch_size)
+        iteration_number = 0
+        jaccard_val_mean = 0
+        jaccard_train_mean = 0
+        for iteration_number in range(1, nb_iterations + 1):
+            try:
+                data_dict_train = sample_data_dict(images, images_masks_stacked, images_data, 'train')
+                model.train_model(data_dict_train, nb_epoch=1, batch_size=batch_size)
 
-            # validate the model
-            if iteration_number % 5 == 0:
-                jaccard_val = evaluate_model_jaccard(model, images, images_masks_stacked, images_data,
-                                                     needed_classes, kind='val')
-                jaccard_train = evaluate_model_jaccard(model, images, images_masks_stacked, images_data,
-                                                       needed_classes, kind='train')
-                jaccard_val_mean = np.mean(jaccard_val)
-                jaccard_train_mean = np.mean(jaccard_train)
+                # validate the model
+                if iteration_number % 5 == 0:
+                    jaccard_val = evaluate_model_jaccard(model, images, images_masks_stacked, images_data,
+                                                         needed_classes, kind='val')
+                    jaccard_train = evaluate_model_jaccard(model, images, images_masks_stacked, images_data,
+                                                           needed_classes, kind='train')
+                    jaccard_val_mean = np.mean(jaccard_val)
+                    jaccard_train_mean = np.mean(jaccard_train)
 
-                logging.info('Iteration %s, jaccard val: %.5f, jaccard train: %.5f',
-                             iteration_number, jaccard_val_mean, jaccard_train_mean)
+                    logging.info('Iteration %s, jaccard val: %.5f, jaccard train: %.5f',
+                                 iteration_number, jaccard_val_mean, jaccard_train_mean)
 
-                for i, cls in enumerate(needed_classes_names):
-                    model.write_scalar_summary('jaccard_val/{}'.format(cls), jaccard_val[i])
-                    model.write_scalar_summary('jaccard_train/{}'.format(cls), jaccard_train[i])
+                    for i, cls in enumerate(needed_classes_names):
+                        model.write_scalar_summary('jaccard_val/{}'.format(cls), jaccard_val[i])
+                        model.write_scalar_summary('jaccard_train/{}'.format(cls), jaccard_train[i])
 
-                model.write_scalar_summary('jaccard_mean/val', jaccard_val_mean)
-                model.write_scalar_summary('jaccard_mean/train', jaccard_train_mean)
+                    model.write_scalar_summary('jaccard_mean/val', jaccard_val_mean)
+                    model.write_scalar_summary('jaccard_mean/train', jaccard_train_mean)
 
-            # save the model
-            if iteration_number % 15 == 0:
-                model_filename = os.path.join(MODELS_DIR, model_name)
-                saved_filename = model.save_model(model_filename)
-                logging.info('Model saved: %s', saved_filename)
+                # save the model
+                if iteration_number % 15 == 0:
+                    model_filename = os.path.join(MODELS_DIR, model_name)
+                    saved_filename = model.save_model(model_filename)
+                    logging.info('Model saved: %s', saved_filename)
 
-        except KeyboardInterrupt:
-            break
+            except KeyboardInterrupt:
+                break
 
-    result = {
-        'iteration': iteration_number,
-        'jaccard_val': jaccard_val_mean,
-        'jaccard_train': jaccard_train_mean,
-    }
-    return result
+        result = {
+            'iteration': iteration_number,
+            'jaccard_val': jaccard_val_mean,
+            'jaccard_train': jaccard_train_mean,
+        }
+        return result
+
+    # predict
+    else:
+        model_to_restore = '{}-{}'.format(model_name, model_load_step)
+        model_filename = os.path.join(MODELS_DIR, model_to_restore)
+        model.restore_model(model_filename)
+        logging.info('Model restored: %s', os.path.basename(model_filename))
+
+        target_images = images_test
+        for img_number, img_id in enumerate(target_images):
+            img_filename = os.path.join(IMAGES_NORMALIZED_DATA_DIR, img_id + '.pkl')
+            img_normalized = load_pickle(img_filename)
+
+            patches, patches_coord = split_image_to_patches([img_normalized, ], [patch_size, ], overlap=0.5)
+
+            X = patches[0]
+
+            data_dict = {'X': X}
+            classes_probs_patches = model.predict(data_dict, batch_size=batch_size)
+
+            classes_probs = join_mask_patches(
+                classes_probs_patches, patches_coord[0],
+                images_metadata[img_id]['height_rgb'], images_metadata[img_id]['width_rgb'],
+                softmax=True, normalization=False)
+
+            masks_without_excluded = convert_softmax_to_masks(classes_probs)
+
+            # join masks and put zeros insted of excluded classes
+            zeros_filler = np.zeros_like(masks_without_excluded[:, :, 0])
+            masks_all = []
+            j = 0
+            for i in range(nb_classes):
+                if i + 1 not in classes_to_skip:
+                    masks_all.append(masks_without_excluded[:, :, j])
+                    j += 1
+                else:
+                    masks_all.append(zeros_filler)
+
+            masks = np.stack(masks_all, axis=-1)
+
+            mask_filename = os.path.join(IMAGES_PREDICTION_MASK_DIR, '{0}_{1}.npy'.format(img_id, model_name))
+            np.save(mask_filename, masks)
+
+            logging.info('Predicted: %s/%s [%.2f]',
+                         img_number + 1, len(target_images), 100 * (img_number + 1) / len(target_images))
+
+        result = {
+
+        }
+        return result
 
 
 if __name__ == '__main__':
